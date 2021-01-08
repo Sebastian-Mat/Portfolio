a4cef68e0749d7580d443c348f36e6e5f9f323be
Sebastian-Mat <67850496+Sebastian-Mat@users.noreply.github.com>
2021-01-08 12:43:56 -0500

    Nav Finished

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
+import React, { useEffect, useRef } from "react";
+
+import "./Normalize.scss";
 import "./App.scss";
+
 import {
   BrowserRouter as Router,
   Route,
   Switch,
   NavLink,
 } from "react-router-dom";
+import { TweenMax, Expo } from "gsap";
+
 import Work from "./components/Work";
 import About from "./components/About";
 import Contact from "./components/Contact";
 
 function App() {
+
+  let nav = useRef(null);
+
+  useEffect(() => {
+      TweenMax.from(nav.childNodes, {
+      y: "-35px",
+      duration: 0.5,
+      delay: 1,
+      ease: Expo.easeInOut,
+      opacity: 0,
+      stagger: {
+        amount:0.1
+      }
+    });
+  }, []);
   return (
     <Router>
-      <div className="nav">
-        <NavLink to="/">Work</NavLink>
-        <NavLink to="/About">About</NavLink>
-        <NavLink to="/Contact">Contact</NavLink>
+      <div className="nav" ref={el => {nav = el}}>
+        <NavLink to="/About" id="link">
+          About
+        </NavLink>
+        <NavLink to="/" id="link" exact>
+          Work
+        </NavLink>
+        <NavLink to="/Contact" id="link">
+          Touch
+        </NavLink>
       </div>
       <div className="screen">
         <Switch>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -9,18 +9,11 @@ const Work = () => {
       opacity: 0,
       ease: Power3.easeOut,
     });
-    TweenMax.from("#img", 1, {
-        y: "20px",
-        delay: 2,
-        opacity: 0,
-        ease: Power3.easeOut,
-      });
   }, []);
 
   return (
     <div className="work">
       <h1 id="work">Work</h1>
-      <img id="img" src="https://images.squarespace-cdn.com/content/v1/5442b6cce4b0cf00d1a3bef2/1599085225108-P8VOM6IL9ERON0B7Z9Q9/ke17ZwdGBToddI8pDm48kJB_XQ3Dc5SllUdWbukQo69Zw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpx6nEhjlnWL4Jp2yRUKnAHfAtKyAEGaEsOmaOT_zZoS2TtcLgPBsTCXYa-xKfKtauE/Most-Prominent-Sustainble-Streetwear-Brands.jpg?format=1500w" alt=""/>
     </div>
   );
 };
